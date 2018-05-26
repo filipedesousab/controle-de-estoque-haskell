c0571559dd1b77e737ece36c7a47f79585f09fc7
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-05-26 17:19:37 -0300

    Opção de listar ":l" adicionada

--- src/Product.hs
+++ src/Product.hs
@@ -54,6 +54,29 @@ newProduct = do { hSetBuffering stdout NoBuffering;
                   getConfirm newProduct;
                 }
 
+-- Simulation Products
+products = [["Produto1", "0001", "20.00", "18"], ["Produto2", "0002", "25.00", "18"], ["Produto3", "0003", "10.00", "18"], ["Produto4", "0004", "22.00", "18"], ["Produto5", "0005", "12.00", "18"], ["Produto6", "0006", "15.00", "18"], ["Produto7", "0007", "85.00", "18"], ["Produto8", "0008", "66.00", "18"], ["Produto9", "0009", "78.00", "18"], ["Produto10", "0010", "33.00", "18"], ["Produto11", "0011", "43.00", "18"], ["Produto12", "0012", "56.00", "18"], ["Produto13", "0013", "44.00", "18"], ["Produto14", "0014", "72.00", "18"], ["Produto15", "0015", "32.00", "18"], ["Produto16", "0016", "81.00", "18"], ["Produto17", "0017", "42.00", "18"], ["Produto18", "0018", "50.00", "18"]]
+
+-- Display product list
+printProduct [] _ = putStr ""
+printProduct (x:xs) i = do {
+  putStr $ "Nome: " ++ x!!0 ++ "\nCódigo: " ++ x!!1 ++ "\nPreço: R$" ++ x!!2 ++ "\nImposto: " ++ x!!3 ++ "%" ++"\n------------------------------\n";
+  if mod i 5 == 0
+    then do {
+      value <- getLine;
+      if value == ":q"
+        then addProduct
+        else printProduct xs (i+1);
+    }
+    else printProduct xs (i+1);
+}
+
+-- Get product list view
+listProducts = do {
+  putStrLn $ msgLayout "Listar produtos\nDigite \":q\" para voltar";
+  printProduct products 1;
+}
+
 {-
 Get quantity of products
 Call putStrLn
@@ -64,11 +87,13 @@ getQuantityAddProduct x = do {
   quantity <- getValue;
   if quantity == ":q"
     then msgComeBack
-    else do {
-      putStr "Confirmar entrada do produto no estoque? [s/n]";
-      getConfirm addProduct;
-      putStrLn(show(x ++ [quantity]));
-    }
+    else if quantity == ":l"
+      then listProducts
+      else do {
+        putStr "Confirmar entrada do produto no estoque? [s/n]";
+        getConfirm addProduct;
+        putStrLn(show(x ++ [quantity]));
+      }
 }
 
 {-
@@ -81,12 +106,14 @@ getCodeAddProduct = do {
   code <- getValue;
   if code == ":q"
     then msgComeBack
-    else getQuantityAddProduct [code]
+    else if code == ":l"
+      then listProducts
+      else getQuantityAddProduct [code]
 }
 
 addProduct :: IO ()
 addProduct = do {
   hSetBuffering stdout NoBuffering;
-  putStrLn $ msgLayout "Adicionar produtos ao estoque\nAqualquer momento digite \":q\" para voltar ao menu principal";
+  putStrLn $ msgLayout "Adicionar produtos ao estoque\nA qualquer momento digite:\n\":q\" para voltar ao menu principal\n\":l\" para listar os produtos";
   getCodeAddProduct;
 }
