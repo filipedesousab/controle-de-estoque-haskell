e728fecbfac9f36c872845d88850233e34f49603
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-05-27 16:52:06 -0300

    Produtos em tuplas

--- src/Product/ListProducts.hs
+++ src/Product/ListProducts.hs
@@ -6,7 +6,7 @@ import Product.Products
 -- Display product list
 printProduct [] _ = putStr ""
 printProduct (x:xs) i = do {
-  putStr $ "Nome: " ++ x!!0 ++ "\nCódigo: " ++ x!!1 ++ "\nPreço: R$" ++ x!!2 ++ "\nImposto: " ++ x!!3 ++ "%" ++"\n------------------------------\n";
+  putStr $ "Nome: " ++ (descriptionProduct x) ++ "\nCódigo: " ++ show(codeProduct x) ++ "\nPreço: R$" ++ show(priceProduct x) ++ "\nImposto: " ++ show(taxProduct x) ++ "%" ++"\n------------------------------\n";
   if mod i 5 == 0
     then do {
       value <- getLine;
@@ -20,5 +20,5 @@ printProduct (x:xs) i = do {
 -- Get product list view
 listProducts = do {
   putStr $ msgPrimary "Listar produtos\nDigite \":q\" para voltar";
-  printProduct products 1;
+  printProduct getProducts 1;
 }
--- src/Product/Products.hs
+++ src/Product/Products.hs
@@ -1,4 +1,11 @@
-module Product.Products (products) where
+module Product.Products (
+    products,
+    getProducts,
+    codeProduct,
+    descriptionProduct,
+    priceProduct,
+    taxProduct
+  ) where
 
 -- Simulation Products
 products = [
@@ -21,3 +28,30 @@ products = [
     ["Produto17", "0017", "42.00", "18"],
     ["Produto18", "0018", "50.00", "18"]
   ]
+
+type Product = (Int, String, Double, Double)
+
+codeProduct :: Product -> Int
+codeProduct (c, _, _, _) = c
+
+descriptionProduct :: Product -> String
+descriptionProduct (_, d, _, _) = d
+
+priceProduct :: Product -> Double
+priceProduct (_, _, p, _) = p
+
+taxProduct :: Product -> Double
+taxProduct (_, _, _, t) = t
+
+getProducts :: [Product]
+getProducts = [
+    (0001, "Produto1", 20.00, 18.00),
+    (0002, "Produto2", 25.00, 18.00),
+    (0003, "Produto3", 10.00, 18.00),
+    (0004, "Produto4", 22.00, 18.00),
+    (0005, "Produto5", 21.00, 18.00),
+    (0006, "Produto6", 15.00, 18.00),
+    (0007, "Produto7", 85.00, 18.00),
+    (0008, "Produto8", 66.00, 18.00)
+  ]
+
