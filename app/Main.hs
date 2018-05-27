6dd9e4f3c601967b68c59968bf2d36ff816b4785
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-05-26 23:54:58 -0300

    Adicionado opção de sair da aplicação

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
+import System.Exit
 import Menus
 import Product.AddProduct
 import Product.NewProduct
@@ -8,15 +9,16 @@ import Product.NewProduct
 main :: IO ()
 main = do {
             mainMenu;
-            op <- getChar;
+            op <- getLine;
             case op of
-              '1' -> do {
+              "1" -> do {
                           addProduct;
                           main;
                         }
-              '4' -> do {
+              "4" -> do {
                           newProduct;
                           main;
                         }
-              _ -> do main
+              ":q" -> die "Até logo!"
+              _ -> main
           }
