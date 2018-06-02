349457445058e0989b8ceb4002172e6aa52bb16c
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-02 00:40:49 -0300

    Ajustes no Database

--- entities.sql
+++ entities.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.7.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 01, 2018 at 10:52 PM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.5
+-- Host: sql10.freemysqlhosting.net
+-- Generation Time: Jun 02, 2018 at 06:37 AM
+-- Server version: 5.5.58-0ubuntu0.14.04.1
+-- PHP Version: 7.0.30-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `testeHaskel`
+-- Database: `sql10240941`
 --
 
 -- --------------------------------------------------------
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `produtos` (
-  `codigo` int(11) DEFAULT '0',
-  `descrecao` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '0',
-  `preco` float DEFAULT '0',
-  `imposto` float DEFAULT '0',
+  `codigo` int(11) NOT NULL,
+  `descricao` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `preco` double DEFAULT '0',
+  `imposto` double DEFAULT '0',
   `quantidade` int(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -40,12 +40,13 @@ CREATE TABLE `produtos` (
 -- Dumping data for table `produtos`
 --
 
-INSERT INTO `produtos` (`codigo`, `descrecao`, `preco`, `imposto`, `quantidade`) VALUES
-(123456, 'Produto1', 25, 18, 0),
-(2, 'Produto2', 20, 18, 0),
-(1, 'Primeiro Produto', 25, 10, 0),
+INSERT INTO `produtos` (`codigo`, `descricao`, `preco`, `imposto`, `quantidade`) VALUES
+(2, 'Produto 2', 20, 18, 0),
+(1, 'Produto 1', 15, 7, 0),
 (3, 'Produto 3', 15, 5, 0),
-(4, 'Produto 4', 55, 5, 0);
+(4, 'Produto 4', 20, 5, 0),
+(157808, 'Produto X', 49.9, 5, 0),
+(984875, 'Produto Y', 79.9, 3, 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/Database/Conn.hs
+++ src/Database/Conn.hs
@@ -3,12 +3,23 @@ module Database.Conn ( connectDatabase ) where
 import Database.HDBC
 import Database.HDBC.MySQL
 
--- Function to get a connection to the database
-connectDatabase :: IO Connection
-connectDatabase = connectMySQL defaultMySQLConnectInfo {
+-- Connection data
+localhost = defaultMySQLConnectInfo {
+    mysqlHost = "127.0.0.1",
+    mysqlUser     = "root",
+    mysqlPassword = "root",
+    mysqlPort = 3306,
+    mysqlDatabase = "projetoHaskell"
+  }
+
+remote = defaultMySQLConnectInfo {
     mysqlHost = "sql10.freemysqlhosting.net",
     mysqlUser     = "sql10240941",
     mysqlPassword = "lyTzclBLLr",
     mysqlPort = 3306,
     mysqlDatabase = "sql10240941"
   }
+
+-- Function to get a connection to the database
+connectDatabase :: IO Connection
+connectDatabase = connectMySQL remote
