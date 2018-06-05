ece11371a1078ba199d92d1ac95beb641be4784f
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-05 20:59:25 -0300

    Tratando entrada de dados do SearchProductByCode.hs

--- src/Product/SearchProductByCode.hs
+++ src/Product/SearchProductByCode.hs
@@ -2,6 +2,8 @@ module Product.SearchProductByCode ( searchProductByCode ) where
 
 import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
 import Database.HDBC
+import Text.Regex.Posix
+import Control.Exception
 
 import Input
 import Layout
@@ -9,6 +11,29 @@ import CustomColors
 import Database.Values
 import Product.Products
 import Product.ListProducts
+
+getCode = do
+  putStr "Digite o código do produto: "
+  code <- getValue
+  if code == ":q"
+    then msgComeBack
+    else if code == ":l"
+      then do
+        listProducts
+        searchProductByCode
+      else if ((code =~ "^[0-9]+$")::Bool)
+      then do
+        product <- getProductByCode (read (code)::Int)
+        if length product > 0
+          then do
+            printProduct $ product!!0
+            putStr $ msgSuccess "Produto localizado com sucesso ✔"
+          else do
+            putStr $ msgWarning "Produto não localizado!"
+            searchProductByCode
+      else do
+        putStr $ msgDanger "Insira um valor inteiro válido!"
+        searchProductByCode
 
 -- Search single product by code
 searchProductByCode :: IO ()
@@ -19,18 +44,9 @@ searchProductByCode = do
   putStrLn $ "\":q\" para voltar ao menu principal"
   putStrLn $ "\":l\" para listar os produtos"
   putStr $ borderLayout ++ colorDefault
-  putStr "Digite o código do produto: "
-  code <- getValue
-  if code == ":q"
-    then msgComeBack
-    else if code == ":l"
-      then do
-        listProducts
-        searchProductByCode
-      else do
-        product <- getProductByCode (read (code)::Int)
-        if length product > 0
-          then do
-            printProduct $ product!!0
-            putStr $ msgSuccess "Produto localizado com sucesso ✔"
-          else putStr $ msgWarning "Produto não localizado!"
+  getCode `catch` msgException
+
+msgException :: IOError -> IO ()
+msgException _ = do
+  putStr $ msgDanger "Entrada inválida!"
+  searchProductByCode
